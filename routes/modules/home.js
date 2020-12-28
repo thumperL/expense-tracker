a9bd9ce1d28d46ba27a5a31b305a24778d9cef27
Thumper <shaunwhl1991@gmail.com>
2020-12-28 19:23:20 +1100

    Add main frame
    Add main frame, including search and filter block

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,11 +1,15 @@
 // 引用 Express 與 Express 路由器
 const express = require('express');
+const Category = require('../../models/category');
 
 const router = express.Router();
 
 // 定義首頁路由
 router.get('/', (req, res) => {
-  res.render('index');
+  Category.find()
+    .lean()
+    .then((categories) => res.render('index', { categories }))
+    .catch((error) => console.error(error));
 });
 // 匯出路由模組
 module.exports = router;
