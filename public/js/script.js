65e66b059d015c771f935956f4fc1816d8b07c1f
Thumper <shaunwhl1991@gmail.com>
2020-12-29 13:46:56 +1100

    Add record edit handling
    Add recordForm to handle new/edit record
    Add records route
    Add javascript frontend form select handling
    Add edit link to index record list

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,20 @@
 $(() => {
+  // Add Stripe to record list
   document.querySelectorAll('.records-wrap .record').forEach((val, key) => {
     if (key % 2 === 1) {
       val.classList.remove('bg-light');
       val.classList.add('bg-secondary');
     }
+  });
+
+  // Set selected for pre-defined option for category in edit/new
+  const defaultCategoryId = document.querySelector('input[name=category]').value;
+  if (document.querySelector('input[name=category]').value !== '') {
+    document.querySelector(`#categorySelect option[value="${defaultCategoryId}"]`).setAttribute('selected', true);
+  }
+
+  // Add event listener to categorySelect
+  document.querySelector('#categorySelect').addEventListener('change', (event) => {
+    document.querySelector('input[name=category]').value = event.target.value;
   });
 });
---
+++ routes/modules/records.js
@@ -0,0 +1,55 @@
+// 引用 Express 與 Express 路由器
+const express = require('express');
+
+const router = express.Router();
+
+// 引用 model 與 validator middleware
+const Category = require('../../models/category');
+const Record = require('../../models/record');
+
+// UPDATE operation
+router.get('/:recordId/edit', (req, res) => {
+  const { recordId } = req.params;
+  const promise = [];
+
+  promise.push(
+    Record.findById(recordId)
+      .lean()
+      .then((records) => {
+        const recordDate = new Date(records.date);
+        records.date = recordDate.toLocaleDateString('en-CA');
+        return records;
+      })
+      .catch((error) => console.log(error)),
+  );
+
+  Promise.all(promise).then((records) => {
+    const recordData = { ...records }[0];
+    Category.find()
+      .lean()
+      .then((categoryData) => res.render('recordForm', { recordData, categoryData }))
+      .catch((error) => console.log(error));
+  });
+});
+router.put('/:recordId', (req, res) => {
+  const { recordId } = req.params;
+  const { name } = req.body;
+  const { date } = req.body;
+  const { amount } = req.body;
+  const { category } = req.body;
+
+  return Record.findById(recordId)
+    .then((record) => {
+      record.name = name;
+      record.date = date;
+      record.amount = amount;
+      record.category = category;
+
+      return record.save();
+    })
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error));
+});
+
+// 匯出路由模組
+module.exports = router;
