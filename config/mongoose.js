c0bf0d20da81cc9ea890b31f90ec4ce4732a683f
Thumper <shaunwhl1991@gmail.com>
2020-12-29 18:57:57 +1100

    Change DB connection setup
    Change DB connection setup

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://root:bjMHZjciE2mLXUVDqA6E@cluster0.sgezw.mongodb.net/expense-tracker?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+// Default connection path
+const connectionRemote = process.env.MONGODB_URI;
+console.log(connectionRemote);
+
+// MongoDB Connection
+mongoose.connect(connectionRemote, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 const db = mongoose.connection;
-db.on('error', () => {
-  console.log('MongoDb Atlas connection error!');
-});
-db.once('open', () => {
-  console.log('MongoDb Atlas [expense-tracker] cluster0 connected!');
-});
+
+// Error Handling
+db.on('error', console.error.bind(console, 'MongoDb Atlas [expense-tracker] cluster0 connection error'));
+
 module.exports = db;
