02fc099dafb014a7fd272329f1d3ff9d90abb557
Thumper <shaunwhl1991@gmail.com>
2021-01-18 18:21:58 +1100

    feat: add user registration feature
    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const User = require('../../models/user');
 
 const router = express.Router();
 router.get('/login', (req, res) => {
@@ -6,5 +7,33 @@ router.get('/login', (req, res) => {
 });
 router.get('/register', (req, res) => {
   res.render('register');
+});
+router.post('/register', (req, res) => {
+  // 取得註冊表單參數
+  const {
+    name, email, password, confirmPassword,
+  } = req.body;
+  // 檢查使用者是否已經註冊
+  User.findOne({ email }).then((user) => {
+    // 如果已經註冊：退回原本畫面
+    if (user) {
+      console.log('User already exists.');
+      res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword,
+      });
+    } else {
+      // 如果還沒註冊：寫入資料庫
+      return User.create({
+        name,
+        email,
+        password,
+      })
+        .then(() => res.redirect('/'))
+        .catch((err) => console.log(err));
+    }
+  });
 });
 module.exports = router;
