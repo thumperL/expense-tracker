fcc3c5a331cf2ab9136e760e19bd8bf822807a98
Thumper <shaunwhl1991@gmail.com>
2021-01-18 19:14:48 +1100

    feat: add bcrypt to register and login
    feat: add bcrypt to register and login

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const passport = require('passport');
+const bcrypt = require('bcryptjs');
 const User = require('../../models/user');
 
 const router = express.Router();
@@ -61,11 +62,14 @@ router.post('/register', (req, res) => {
         confirmPassword,
       });
     }
-    return User.create({
-      name,
-      email,
-      password,
-    })
+    return bcrypt
+      .genSalt(10)
+      .then((salt) => bcrypt.hash(password, salt))
+      .then((hash) => User.create({
+        name,
+        email,
+        password: hash,
+      }))
       .then(() => res.redirect('/'))
       .catch((err) => console.log(err));
   });
