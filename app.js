e84e7580fcb32be276dc473e0f3d04d343ad7909
Thumper <shaunwhl1991@gmail.com>
2021-01-18 19:09:29 +1100

    feat: add flash message
    feat: add flash message

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override'); // 載入 method-override
+const flash = require('connect-flash');
 const routes = require('./routes'); // 引用路由器
 const usePassport = require('./config/passport');
 require('./config/mongoose');
@@ -31,9 +32,12 @@ app.use(bodyParser.urlencoded({
 // 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'));
 usePassport(app);
+app.use(flash());
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.user = req.user;
+  res.locals.success_msg = req.flash('success_msg'); // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg'); // 設定 warning_msg 訊息
   next();
 });
 // Route to handle routing
