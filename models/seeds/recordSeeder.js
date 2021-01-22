cfe8e8444de5875bd8fd938b1caf4a061c1a12e3
thumperL <shaunwhl1991@gmail.com>
2021-01-22 18:14:33 +1100

    refactor: recordSeeder promisify
    refactor: recordSeeder promisify

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -8,8 +8,6 @@ const Record = require('../record');
 const User = require('../user');
 const db = require('../../config/mongoose');
 
-console.log('MongoDb Atlas [expense-tracker] cluster0 connected! @ recordSeeder');
-
 const seedUsers = [
   {
     name: 'Sample User 1',
@@ -23,46 +21,62 @@ const seedUsers = [
   },
 ];
 
-const categoriesCreated = [];
-let userCreateCount = 0;
-const promise = new Promise((resolve, reject) => {
-  seedUsers.forEach((eaSeedUser, index) => {
-    Category.find()
-      .lean()
-      .then((categories) => categoriesCreated.push(...categories))
-      .then(() => bcrypt.genSalt(10))
-      .then((salt) => bcrypt.hash(eaSeedUser.password, salt))
-      .then((hash) => User.create({
-        name: eaSeedUser.name,
-        email: eaSeedUser.email,
-        password: hash,
-      }))
-      .then((user) => {
-        const userId = user._id;
-        return Promise.all(
-          Array.from(
-            { length: 5 },
-            (_, i) => Record.create(
-              {
-                name: `record-${i}`,
-                merchant: `merchant-${i}`,
-                category: categoriesCreated[Math.floor(Math.random() * (categoriesCreated.length - 1))]._id,
-                amount: Math.floor(Math.random() * 1000) + 1,
-                userId,
-              },
-            ),
-          ),
-        );
-      })
-      .then(() => {
-        userCreateCount += 1;
-        if (userCreateCount === seedUsers.length) {
-          resolve();
-        }
+const getCategories = () => new Promise((resolve, reject) => {
+  Category
+    .find()
+    .lean()
+    .then((categories) => resolve(categories))
+    .catch((error) => reject(`[Category Find Error]: ${error}`));
+});
+
+const createUser = (user) => new Promise((resolve, reject) => {
+  bcrypt.genSalt(10)
+    .then((salt) => bcrypt.hash(user.password, salt))
+    .then((hash) => User.create({
+      name: user.name,
+      email: user.email,
+      password: hash,
+    }))
+    .then((userCreated) => resolve(userCreated))
+    .catch((error) => reject(`[User Create Error]: ${error}`));
+});
+
+async function start() {
+  try {
+    const categories = await getCategories();
+
+    const createRecord = () => new Promise((resolve, reject) => {
+      seedUsers.forEach((eaSeedUser, index) => {
+        createUser(eaSeedUser)
+          .then((user) => {
+            const userId = user._id;
+            return Promise.all(
+              Array.from(
+                { length: 5 },
+                (_, i) => Record.create(
+                  {
+                    name: `record-${i}`,
+                    merchant: `merchant-${i}`,
+                    category: categories[Math.floor(Math.random() * (categories.length - 1))]._id,
+                    amount: Math.floor(Math.random() * 1000) + 1,
+                    userId,
+                  },
+                ),
+              ),
+            );
+          })
+          .then(() => {
+            if (index === seedUsers.length - 1) resolve();
+          });
       });
-  });
-});
-promise.then(() => {
-  console.log('Seeder Import Completed.');
-  process.exit();
-});
+    });
+    createRecord().then(() => {
+      console.log('Seeder Import Completed.');
+      process.exit();
+    });
+  } catch (e) {
+    console.warn(e);
+  }
+}
+
+start();
